65b0aae24dbc6b9a863af35be5169bdf173f4341
Richard S. Graham <RichGraham@users.noreply.github.com>
2022-12-12 11:34:54 +0000

    Renamed test

--- Tests.py
+++ Tests.py
@@ -2,7 +2,7 @@ import unittest
 import exponential
 class Initialise(unittest.TestCase):
 
-    def testExponetial(self):
+    def test_at_x_equals_1(self):
         actual = exponential.eval_exp(1.0, n_terms=10)
         expected = 2.7182818284590452353602874713526624977572470936
         self.assertAlmostEqual(actual , expected)
